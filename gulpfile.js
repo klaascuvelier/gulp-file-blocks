17bd3d80ec4204e3d555ff36cdd100856eefe10d
Klaas Cuvelier <cuvelierklaas@gmail.com>
2014-09-30 10:46:14 +0200

    start tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,14 @@
 'use strict';
 
+var gulp    = require('gulp'),
+    jshint  = require('gulp-jshint'),
+    jasmine = require('gulp-jasmine');
 
-gulp.task('default')
+gulp.task('lint', function() {
+    return gulp.src('spec/main.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(jasmine());
+});
+
+gulp.task('default', ['lint']);
---
+++ spec/main.js
@@ -0,0 +1,12 @@
+/* jslint node: true */
+/* global describe, it, expect */
+'use strict';
+
+var _           = require('lodash'),
+    FileBlocks  = require('../index.js');
+
+describe('FileBlocks', function () {
+    it('Should be defined', function () {
+        expect(FileBlocks).toBeDefined();
+    });
+});
